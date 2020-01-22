7c17c6c9e7252254a3e8bcccbf8578f7d056bcba
Matías Lang <matiasl@infobytesec.com>
2020-01-22 17:24:00 -0300

    Add nix expression to parse setup.py requirements

---
+++ parse_setuppy_data.nix
@@ -0,0 +1,24 @@
+{ file, stdenv ? (import <nixpkgs> { }).stdenv
+, python ? (import <nixpkgs> { }).python3 }:
+
+let
+  removeExt = fileName: builtins.elemAt (builtins.split "\\." fileName) 0;
+
+  patchedSetuptools = python.pkgs.setuptools.overrideAttrs (ps: {
+    # src = (import <nixpkgs> {}).lib.cleanSource ./setuptools;
+    patches = [ ./setuptools_patch.diff ];
+  });
+
+  pythonWithPackages = python.withPackages (ps: [ patchedSetuptools ]);
+
+in stdenv.mkDerivation {
+  name = "setup.py_data_${removeExt (builtins.baseNameOf file)}";
+  src = file;
+  buildInputs = [ pythonWithPackages ];
+  buildPhase = ''
+    mkdir -p $out
+    PYPI2NIXKPGS=1 python setup.py install
+  '';
+  dontInstall = true;
+}
+
