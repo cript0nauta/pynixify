adaa75a3ccc23706c0bc1c0d5cab4d3f3923cde9
Matías Lang <matiasl@infobytesec.com>
2020-01-27 15:19:42 -0300

    Use pytest for acceptance tests

--- acceptance_tests/test_download_from_pypi.py
+++ acceptance_tests/test_download_from_pypi.py
@@ -1,22 +1,18 @@
 import os
 import shutil
-import asyncio
+import pytest
 from packaging.requirements import Requirement
 from pypi2nixpkgs.pypi_api import (
     PyPIData,
     PyPICache,
 )
 
-async def main():
+@pytest.mark.asyncio
+async def test_download_from_pypi():
     data = PyPIData(PyPICache())
     req = Requirement('faraday-agent_dIspatcher==1.0')
     (package, ) = await data.from_requirement(req)
     print(package)
     assert package.sha256 == 'd7a549af9047f1af0d824cf29dd93c5449241266a24e8687d3399cb15bfc61ae'
     path = await package.download_source()
-    shutil.copy(path, '/tmp/package.tar.gz')
     os.remove(path)
-
-
-if __name__ == '__main__':
-    asyncio.run(main())
