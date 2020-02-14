e3fc07b89356c4d2c4619b8e513088b4885ff05f
Matías Lang <matiasl@infobytesec.com>
2020-02-14 14:33:40 -0300

    Use a cleaned source in parse_setuppy_data.nix
    Otherwise it would create a similar derivation on each run of
    pypi2nixpkgs.

--- parse_setuppy_data.nix
+++ parse_setuppy_data.nix
@@ -1,4 +1,4 @@
-{ file, stdenv ? (import <nixpkgs> { }).stdenv
+{ file, stdenv ? (import <nixpkgs> { }).stdenv, lib ? (import <nixpkgs> { }).lib
 , unzip ? (import <nixpkgs> { }).unzip, python ? (import <nixpkgs> { }).python3
 }:
 
@@ -12,9 +12,17 @@ let
 
   pythonWithPackages = python.withPackages (ps: [ patchedSetuptools ]);
 
+  cleanSource = src:
+    lib.cleanSourceWith {
+      filter = name: type:
+        lib.cleanSourceFilter name type && builtins.baseNameOf (toString name)
+        != "pypi2nixpkgs";
+      inherit src;
+    };
+
 in stdenv.mkDerivation {
   name = "setup.py_data_${removeExt (builtins.baseNameOf file)}";
-  src = file;
+  src = cleanSource file;
   nativeBuildInputs = [ unzip ];
   buildInputs = [ pythonWithPackages ];
   buildPhase = ''
