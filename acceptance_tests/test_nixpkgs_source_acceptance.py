cript0nauta/pynixify

commit 0db0766774fb50932e265973eae108a95f97df7a

Set the correct package version when using --local

pynixify/base.py
@@ -58,4 +58,13 @@ class Package:
                 license=None,
             )
         with (nix_store_path / 'meta.json').open() as fp:
-            return PackageMetadata(**json.load(fp))
+            metadata = json.load(fp)
+            try:
+                version: str = metadata.pop('version')
+            except KeyError:
+                pass
+            else:
+                # When using --local, version starts hardcoded to 0.1dev. This
+                # will update it to its real value
+                self.version = Version(version)
+            return PackageMetadata(**metadata)

acceptance_tests/test_nixpkgs_source_acceptance.py
@@ -19,7 +19,7 @@ import asyncio
 import tempfile
 from pathlib import Path
 from packaging.requirements import Requirement
-from packaging.version import parse
+from packaging.version import Version, parse
 from pynixify.nixpkgs_sources import (
     load_nixpkgs_data,
     NixpkgsData,
@@ -194,7 +194,9 @@ async def test_build_sampleproject_nixpkgs():
         test_requirements=[c.package_for('pytest')],  # type: ignore
         runtime_requirements=[c.package_for('peppercorn')]  # type: ignore
     )
+    package.version = Version('1.2.3')
     meta = await package.metadata()
+    assert package.version == Version('1.3.1')
     sampleproject_expr = build_nix_expression(
         package, reqs, meta, sha256)
 

acceptance_tests/test_command.sh
@@ -35,9 +35,11 @@ teardown(){
 @test "sampleproject-local" {
     git clone https://github.com/pypa/sampleproject
     cd sampleproject
+    git checkout 52966defd6a61e97295b0bb82cd3474ac3e11c7a
     sed -i 's/your/my/' src/sample/__init__.py
     pynixify --local sampleproject
     nix-build pynixify/nixpkgs.nix -A python3.pkgs.sampleproject
+    readlink result | tee | grep 'sampleproject-2.0.0'
     ./result/bin/sample | grep 'Call my main'
     nix-shell pynixify/shell.nix --command sample | grep 'Call my main'
 }

nix/packages/pynixify/default.nix
@@ -7,7 +7,7 @@
 
 buildPythonPackage rec {
   pname = "pynixify";
-  version = "0.1dev";
+  version = "0.1";
 
   src = lib.cleanSource ../../..;
 

pynixify/data/setuptools_patch.diff
@@ -35,7 +35,7 @@ index a71b2bbd..3dbb72bb 100644
  
 +        import json
 +        meta = {}
-+        meta_attrs = {'description', 'url', 'license'}
++        meta_attrs = {'description', 'url', 'license', 'version'}
 +        for meta_attr in meta_attrs:
 +            meta[meta_attr] = attrs.get(meta_attr)
 +        with (out / 'meta.json').open('w') as fp:

setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 setup(
     name='pynixify',
-    version='0.1dev',
+    version='0.1',
     packages=['pynixify',],
     package_data={'pynixify': ['data/*']},
     license='GPLv3+',

commit e77dd4990a04173c99fb2098f4df7444fa1b549e

Fix bug packaging projects without version in the setup.py
Commit 0db0766774fb50932e265973eae108a95f97df7a introduced a tricky
regression: some packages, like daiquiri, don't indicate their version
number to setup.py. So the version was incorrectly being set to None
when it should have used the previous value.

pynixify/base.py
@@ -68,11 +68,12 @@ class Package:
         with (nix_store_path / 'meta.json').open() as fp:
             metadata = json.load(fp)
             try:
-                version: str = metadata.pop('version')
+                version: Optional[str] = metadata.pop('version')
             except KeyError:
                 pass
             else:
-                # When using --local, version starts hardcoded to 0.1dev. This
-                # will update it to its real value
-                self.version = Version(version)
+                if version:  # it might be None
+                    # When using --local, version starts hardcoded to 0.1dev. This
+                    # will update it to its real value
+                    self.version = Version(version)
             return PackageMetadata(**metadata)

acceptance_tests/test_nixpkgs_source_acceptance.py
@@ -110,6 +110,22 @@ async def test_parse_requirements_nixpkgs_with_wheel_source():
     await pkg.metadata()
 
 
+@pytest.mark.asyncio
+async def test_metadata_with_null_version():
+    data = await load_nixpkgs_data(PINNED_NIXPKGS_ARGS)
+    nixpkgs = NixpkgsData(data)
+    pypi = PyPIData(PyPICache())
+    async def f(pkg):
+        return await evaluate_package_requirements(pkg, PINNED_NIXPKGS_ARGS)
+    c = VersionChooser(nixpkgs, pypi, f)
+    await c.require(Requirement('daiquiri==2.1.1'))
+    pkg = c.package_for('daiquiri')
+    assert pkg is not None
+    await pkg.metadata()
+    assert pkg.version == Version('2.1.1')
+
+
+
 async def run_nix_build(expr: str) -> Path:
     proc = await asyncio.create_subprocess_exec(
         'nix-build', '-Q', '-E', '-',