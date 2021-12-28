6a26c5074ecf308a4f44acb04b5b7182b853a57c
Matías Lang <yo@matiaslang.me>
2021-12-28 15:41:31 -0300

    Add mypy stubs to dev environment
    And re-run pynixify

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -12,12 +12,16 @@ let
     python36 = super.python36.override { inherit packageOverrides; };
     python37 = super.python37.override { inherit packageOverrides; };
     python38 = super.python38.override { inherit packageOverrides; };
+    python39 = super.python39.override { inherit packageOverrides; };
+    python310 = super.python310.override { inherit packageOverrides; };
   };
 
   nixpkgs = <nixpkgs>;
 
   packageOverrides = self: super: {
     pynixify = self.callPackage ./packages/pynixify { };
+
+    types-aiofiles = self.callPackage ./packages/types-aiofiles { };
 
   };
 
