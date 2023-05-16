0de114474826a34b12e46e13e596a296dd610cfd
Emmett Butler <emmett.butler321@gmail.com>
2023-05-16 11:12:33 -0700

    make dependencies check attempt both setuptools and hatch builds

--- pynixify/data/parse_setuppy_data.nix
+++ pynixify/data/parse_setuppy_data.nix
@@ -1,43 +1,105 @@
-{ file, stdenv ? (import <nixpkgs> { }).stdenv, lib ? (import <nixpkgs> { }).lib
-, unzip ? (import <nixpkgs> { }).unzip, python ? (import <nixpkgs> { }).python3
-}:
+{ file, pkgs ? import <nixpkgs> { } }:
 
 let
   removeExt = fileName: builtins.elemAt (builtins.split "\\." fileName) 0;
 
-  patchedSetuptools = python.pkgs.setuptools.overrideAttrs (ps: {
+  patchedSetuptools = pkgs.python3.pkgs.setuptools.overrideAttrs (ps: {
     # src = (import <nixpkgs> {}).lib.cleanSource ./setuptools;
 
     patches = [ ./setuptools_patch.diff ];
-    patchFlags = lib.optionals (lib.versionOlder "61" python.pkgs.setuptools.version) ["--merge" "-p1"];
+    patchFlags = pkgs.lib.optionals
+      (pkgs.lib.versionOlder "61" pkgs.python3.pkgs.setuptools.version) [
+        "--merge"
+        "-p1"
+      ];
 
   });
 
-  pythonWithPackages = python.withPackages (ps: [ patchedSetuptools ]);
+  setuptoolsscm = pkgs.python3.pkgs.buildPythonPackage rec {
+    pname = "setuptools-scm";
+    version = "7.0.5";
+
+    src = pkgs.python3.pkgs.fetchPypi {
+      pname = "setuptools_scm";
+      inherit version;
+      sha256 = "sha256-Ax4Tr3cdb4krlBrbbqBFRbv5Hrxc5ox4qvP/9uH7SEQ=";
+    };
+
+    propagatedBuildInputs = [
+      pkgs.python3.pkgs.packaging
+      pkgs.python3.pkgs.typing-extensions
+      pkgs.python3.pkgs.tomli
+      patchedSetuptools
+    ];
+
+    pythonImportsCheck = [ "setuptools_scm" ];
+
+    # check in passthru.tests.pytest to escape infinite recursion on pytest
+    doCheck = false;
+  };
+  hatchvcs = pkgs.python3.pkgs.buildPythonPackage rec {
+    pname = "hatch-vcs";
+    version = "0.2.0";
+    format = "pyproject";
+
+    disabled = pkgs.python3.pkgs.pythonOlder "3.7";
+
+    src = pkgs.python3.pkgs.fetchPypi {
+      pname = "hatch_vcs";
+      inherit version;
+      sha256 = "sha256-mRPXM7NO7JuwNF0GJsoyFlpK0t4V0c5kPDbQnKkIq/8=";
+    };
+
+    nativeBuildInputs = [ pkgs.python3.pkgs.hatchling ];
+
+    propagatedBuildInputs = [ pkgs.python3.pkgs.hatchling setuptoolsscm ];
+
+    checkInputs = [ pkgs.git pkgs.python3.pkgs.pytestCheckHook ];
+
+    disabledTests = [
+      # incompatible with setuptools-scm>=7
+      # https://github.com/ofek/hatch-vcs/issues/8
+      "test_write"
+    ];
+
+    pythonImportsCheck = [ "hatch_vcs" ];
+  };
+
+  pythonWithPackages = pkgs.python3.withPackages
+    (ps: [ patchedSetuptools pkgs.python3.pkgs.hatchling hatchvcs ]);
 
   cleanSource = src:
-    lib.cleanSourceWith {
+    pkgs.lib.cleanSourceWith {
       filter = name: type:
-        lib.cleanSourceFilter name type && builtins.baseNameOf (toString name)
-        != "pynixify";
+        pkgs.lib.cleanSourceFilter name type
+        && builtins.baseNameOf (toString name) != "pynixify";
       name = builtins.baseNameOf src;
       inherit src;
     };
 
-in stdenv.mkDerivation {
+in pkgs.stdenv.mkDerivation {
   name = "setup.py_data_${removeExt (builtins.baseNameOf file)}";
   src = cleanSource file;
-  nativeBuildInputs = [ unzip ];
-  buildInputs = [ pythonWithPackages ];
+  nativeBuildInputs = [ pkgs.unzip ];
+  buildInputs = [ pythonWithPackages pkgs.hatch ];
   configurePhase = ''
     true  # We don't want to execute ./configure
   '';
   buildPhase = ''
     mkdir -p $out
-    if ! PYNIXIFY=1 python setup.py install; then
-      # Indicate that fetching the result failed, but let the build succeed
-      touch $out/failed
+    if PYNIXIFY=1 python setup.py install; then
+        exit 0
     fi
+    if test -f pyproject.toml && grep "hatchling.build" pyproject.toml; then
+        echo 'mode = "local"' > config.toml
+        #echo '[build-system]' >> config.toml
+        #echo 'build-backend = "hatchling.build"' >> config.toml
+        cat config.toml
+        hatch --config config.toml --data-dir $out/data --cache-dir $out/cache build
+        exit 0
+    fi
+    # Indicate that fetching the result failed, but let the build succeed
+    touch $out/failed
   '';
   dontInstall = true;
 }
--- pynixify/nixpkgs_sources.py
+++ pynixify/nixpkgs_sources.py
@@ -126,6 +126,7 @@ async def _run_nix_build(*args: Sequence[str], retries=0, max_retries=5) -> Path
         stderr=asyncio.subprocess.PIPE,
     )
     (stdout, stderr) = await proc.communicate()
+    # sys.stdout.write("%s\n%s" % (stdout.decode(), stderr.decode()))
     status = await proc.wait()
 
     if b"all build users are currently in use" in stderr and retries < max_retries:
@@ -135,7 +136,7 @@ async def _run_nix_build(*args: Sequence[str], retries=0, max_retries=5) -> Path
             f"warning: All build users are currently in use. "
             f"Retrying in {2**retries} seconds\n"
         )
-        await asyncio.sleep(2**retries)
+        await asyncio.sleep(2 ** retries)
         return await run_nix_build(*args, retries=retries + 1, max_retries=max_retries)
     elif retries >= max_retries:
         sys.stderr.write(f"error: Giving up after {max_retries} failed retries\n")
