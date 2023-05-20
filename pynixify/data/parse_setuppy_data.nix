f46bf326580ab6f7ee254578f11c1bafe6601389
Emmett Butler <emmett.butler321@gmail.com>
2023-05-20 07:43:19 -0700

    add basic support for pep517 build backends
    current supported backends are hatchling and flit_core

--- pynixify/data/parse_setuppy_data.nix
+++ pynixify/data/parse_setuppy_data.nix
@@ -21,8 +21,88 @@ let
       ];
 
   });
+  setuptoolsscm = python.pkgs.buildPythonPackage rec {
+    pname = "setuptools-scm";
+    version = "7.0.5";
 
-  pythonWithPackages = python.withPackages (ps: [ patchedSetuptools ]);
+    src = python.pkgs.fetchPypi {
+      pname = "setuptools_scm";
+      inherit version;
+      sha256 = "sha256-Ax4Tr3cdb4krlBrbbqBFRbv5Hrxc5ox4qvP/9uH7SEQ=";
+    };
+
+    propagatedBuildInputs = [
+      python.pkgs.packaging
+      python.pkgs.typing-extensions
+      python.pkgs.tomli
+      patchedSetuptools
+    ];
+
+    pythonImportsCheck = [ "setuptools_scm" ];
+
+    # check in passthru.tests.pytest to escape infinite recursion on pytest
+    doCheck = false;
+  };
+  hatchling = python.pkgs.hatchling.overrideAttrs
+    (ps: { patches = [ ./hatchling_patch.diff ]; });
+  hatchvcs = python.pkgs.buildPythonPackage rec {
+    pname = "hatch-vcs";
+    version = "0.2.0";
+    format = "pyproject";
+
+    disabled = python.pkgs.pythonOlder "3.7";
+
+    src = python.pkgs.fetchPypi {
+      pname = "hatch_vcs";
+      inherit version;
+      sha256 = "sha256-mRPXM7NO7JuwNF0GJsoyFlpK0t4V0c5kPDbQnKkIq/8=";
+    };
+
+    nativeBuildInputs = [ hatchling ];
+
+    propagatedBuildInputs = [ hatchling setuptoolsscm ];
+
+    checkInputs = [ pkgs.git python.pkgs.pytestCheckHook ];
+
+    disabledTests = [
+      # incompatible with setuptools-scm>=7
+      # https://github.com/ofek/hatch-vcs/issues/8
+      "test_write"
+    ];
+
+    pythonImportsCheck = [ "hatch_vcs" ];
+  };
+  patchedflitcore = python.pkgs.flit-core.overrideAttrs
+    (ps: { patches = [ ./flitcore_patch.diff ]; });
+  flitscm = python.pkgs.buildPythonPackage rec {
+    pname = "flit-scm";
+    version = "1.7.0";
+
+    format = "pyproject";
+
+    src = pkgs.fetchFromGitLab {
+      owner = "WillDaSilva";
+      repo = "flit_scm";
+      rev = version;
+      sha256 = "sha256-K5sH+oHgX/ftvhkY+vIg6wUokAP96YxrTWds3tnEtyg=";
+      leaveDotGit = true;
+    };
+
+    nativeBuildInputs =
+      [ patchedflitcore setuptoolsscm python.pkgs.tomli pkgs.git ];
+    propagatedBuildInputs = [ patchedflitcore setuptoolsscm ]
+      ++ pkgs.lib.optionals (python.pkgs.pythonOlder "3.11")
+      [ python.pkgs.tomli ];
+  };
+
+  pythonWithPackages = python.withPackages (ps: [
+    patchedSetuptools
+    setuptoolsscm
+    hatchling
+    hatchvcs
+    flitscm
+    python.pkgs.pip
+  ]);
 
   cleanSource = src:
     lib.cleanSourceWith {
@@ -43,10 +123,14 @@ in stdenv.mkDerivation {
   '';
   buildPhase = ''
     mkdir -p $out
-    if ! PYNIXIFY=1 python setup.py install; then
-      # Indicate that fetching the result failed, but let the build succeed
-      touch $out/failed
+    if PYNIXIFY=1 python setup.py install; then
+        exit 0
     fi
+    if ${python.pkgs.pip}/bin/pip --no-cache-dir wheel --config-settings PYNIXIFY_OUT=$out --no-build-isolation $PWD; then
+        exit 0
+    fi
+    # Indicate that fetching the result failed, but let the build succeed
+    touch $out/failed
   '';
   dontInstall = true;
 }
